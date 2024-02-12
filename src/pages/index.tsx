5339691bff21c99757303e8d191ab7a2f31f6304
hyo814 <ggamjige8888@naver.com>
2024-02-12 13:36:22 +0900

    [24-02-12] 온보딩 화면 2차 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
 import LogoImage from '../../public/assets/Logo.png';
+import OnBoarding from '../../public/assets/animation.gif';
 import Head from 'next/head';
 import Header from "../components/Header";
 
@@ -14,7 +15,7 @@ const Home: () => JSX.Element = () => {
 			<Head>
 				<title>직짱건강 | Home</title>
 			</Head>
-			<div className={styles.header}E>
+			<div className={styles.header} E>
 				<LogoImageView src={LogoImage} alt="로고" width={100} height={100}/>
 				<div>직<strong>짱</strong>건강</div>
 				<Header/>
@@ -23,7 +24,9 @@ const Home: () => JSX.Element = () => {
 				<div className={styles.content}>
 					<div className={styles.content_basic}>책상에 앉아 일을 많이 하는 당신!</div>
 					<div className={styles.content_bold}>현재 건강 상태가 어떤지 체크해볼까요?</div>
-					<div className={styles.image}>이미지</div>
+					<div className={styles.image_container}> {/* CSS 모듈 클래스 적용 */}
+						<Image src={OnBoarding} alt="온보딩"/>
+					</div>
 					<button className={styles.check}>체크 하러 가기</button>
 				</div>
 			</div>
