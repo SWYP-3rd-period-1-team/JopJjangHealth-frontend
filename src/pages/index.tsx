3bdcba321a0a9450b07e340bd67c1052af48047f
hyo814 <ggamjige8888@naver.com>
2024-02-12 15:26:25 +0900

    [24-02-12] 건강 설문 기본 틀 1단계 완성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,20 @@
 import styles from '../styles/Home.module.css';
 import React from 'react';
-import styled from 'styled-components';
 import Image from 'next/image';
-import LogoImage from '../../public/assets/Logo.png';
-import OnBoarding from '../../public/assets/animation.gif';
-import Head from 'next/head';
-import Header from "../components/Header";
-
-const LogoImageView = styled(Image)``;
+import OnBoarding from '../../public/assets/Animation.gif';
+import Layout from "../components/Layout";
+import {router} from "next/client";
 
 const Home: () => JSX.Element = () => {
 	return (
-		<div className={styles.main}>
-			<Head>
-				<title>직짱건강 | Home</title>
-			</Head>
-			<div className={styles.header} E>
-				<LogoImageView src={LogoImage} alt="로고" width={100} height={100}/>
-				<div>직<strong>짱</strong>건강</div>
-				<Header/>
+		<Layout>
+			<div className={styles.content_basic}>책상에 앉아 일을 많이 하는 당신!</div>
+			<div className={styles.content_bold}>현재 건강 상태가 어떤지 체크해볼까요?</div>
+			<div className={styles.image_container}> {/* CSS 모듈 클래스 적용 */}
+				<Image src={OnBoarding} alt="온보딩"/>
 			</div>
-			<div className={styles.section}>
-				<div className={styles.content}>
-					<div className={styles.content_basic}>책상에 앉아 일을 많이 하는 당신!</div>
-					<div className={styles.content_bold}>현재 건강 상태가 어떤지 체크해볼까요?</div>
-					<div className={styles.image_container}> {/* CSS 모듈 클래스 적용 */}
-						<Image src={OnBoarding} alt="온보딩"/>
-					</div>
-					<button className={styles.check}>체크 하러 가기</button>
-				</div>
-			</div>
-		</div>
+			<button className={styles.check} onClick={() => router.push("/Survey")}>체크 하러 가기</button>
+		</Layout>
 	);
 };
 
