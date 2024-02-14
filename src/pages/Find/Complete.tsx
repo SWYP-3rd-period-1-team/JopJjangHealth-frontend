5d215a13459329c843ea72baf248123dd01877a3
hyo814 <ggamjige8888@naver.com>
2024-02-14 15:18:13 +0900

    [24-02-14] 리뷰 데이 전 캡쳐본 이슈 수정

--- src/pages/Find/Complete.tsx
+++ src/pages/Find/Complete.tsx
@@ -5,7 +5,7 @@ const Complete: () => JSX.Element = () => {
 
 	return (
 			<div className={styles.findContainer}>
-				<div className={styles.find_complete}>회원님의 임시 비밀번호는 ${"0000000"} 입니다.</div>
+				<div className={styles.find_complete}>회원님의 임시 비밀번호는 {"aBcd123!"} 입니다.</div>
 				<div className={styles.find_complete}>로그인 후에 꼭 비밀번호를 변경해주세요</div>
 				<button className={styles.findButton}>로그인 하기</button>
 			</div>
