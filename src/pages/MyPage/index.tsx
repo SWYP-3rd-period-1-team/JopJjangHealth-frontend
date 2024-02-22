590d82f493c86b54f6c97fb3108fed1ca3607e9b
hyo814 <ggamjige8888@naver.com>
2024-02-23 02:27:49 +0900

    [24-02-23] 마이페이지 틀 수정

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import Layout from "../../components/Layout";
+import styles from './MyPage.module.css'; // Assume you have a CSS module for styling
 
 const MyPage = () => {
+	const userProfile = {
+		username: '직짱인 님',
+		userId: 'abcde123'
+	};
+	
 	return (
-		<Layout>
-			<div>여기는 마이페이지!</div>
-		</Layout>
+		<div className={styles.myPageContainer}>
+			<div className={styles.profileContainer}>
+				<div className={styles.profileImage}>
+				</div>
+				<div className={styles.profileInfo}>
+					<div className={styles.username}>{userProfile.username}</div>
+					<div className={styles.userId}>{userProfile.userId}</div>
+				</div>
+			</div>
+			<div className={styles.likedListContainer}>
+				<div className={styles.likedTitle}>내가 찜한 리스트</div>
+			</div>
+		</div>
 	);
 };
 
