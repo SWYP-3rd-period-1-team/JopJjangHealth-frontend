a5e6a33fafe5356a93953cee5528b62b86aa3335
hyo814 <ggamjige8888@naver.com>
2024-02-25 17:07:34 +0900

    [24-02-25] 마이페이지 시작

---
+++ src/pages/MyPage/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from '../../styles/MyPage.module.css';
+
+const MyPage = () => {
+	const userProfile = {
+		username: '직짱인 님',
+		userId: 'abcde123'
+	};
+	
+	return (
+		<div className={styles.myPageContainer}>
+			<div className={styles.profileContainer}>
+				<div className={styles.profileImage}>
+				</div>
+				<div className={styles.profileInfo}>
+					<div className={styles.username}>{userProfile.username}</div>
+					<div className={styles.userId}>{userProfile.userId}</div>
+				</div>
+			</div>
+			<div className={styles.likedListContainer}>
+				<div className={styles.likedTitle}>내가 찜한 리스트</div>
+			</div>
+		</div>
+	);
+};
+
+export default MyPage;
